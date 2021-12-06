8c1ba8c82c9cb30f51d08398a7277217ba8412a4
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-06 12:35:28 +0900

    fix: 使わないものを削除

--- lib/component/grass_cell_item.dart
+++ lib/component/grass_cell_item.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:glassmorphism/glassmorphism.dart';
-
 import '../model/todo.dart';
 
 class GrassCellItem extends StatelessWidget {
@@ -17,47 +15,21 @@ class GrassCellItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GlassmorphicFlexContainer(
-      borderRadius: 20,
-      blur: 0,
-      padding: const EdgeInsets.all(10),
-      alignment: Alignment.bottomCenter,
-      border: 2,
-      linearGradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            const Color(0xFFffffff).withOpacity(0.1),
-            const Color(0xFFFFFFFF).withOpacity(0.05),
-          ],
-          stops: const [
-            0.1,
-            1,
-          ]),
-      borderGradient: LinearGradient(
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        colors: [
-          const Color(0xFFffffff).withOpacity(0.5),
-          const Color((0xFFFFFFFF)).withOpacity(0.5),
-        ],
+    return ListTile(
+      //onTap: onTap, // TODO:RouterにIDを渡してタスク編集画面に遷移
+      title: Text(
+        todo.description,
+        style: todo.done
+            ? const TextStyle(
+                color: Colors.grey,
+                fontSize: 30,
+                decoration: TextDecoration.lineThrough,
+              )
+            : const TextStyle(fontSize: 30),
       ),
-      child: ListTile(
-        //onTap: onTap, // TODO:RouterにIDを渡してタスク編集画面に遷移
-        title: Text(
-          todo.description,
-          style: todo.done
-              ? const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 30,
-                  decoration: TextDecoration.lineThrough,
-                )
-              : const TextStyle(fontSize: 30),
-        ),
-        trailing: IconButton(
-          onPressed: onTapDelete, // TODO:削除確認用のバナーを表示させる
-          icon: const Icon(Icons.delete),
-        ),
+      trailing: IconButton(
+        onPressed: onTapDelete, // TODO:削除確認用のバナーを表示させる
+        icon: const Icon(Icons.delete),
       ),
     );
   }
